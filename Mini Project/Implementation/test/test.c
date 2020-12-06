c681e828e294c4ca64b4dc567a5f6186c8b27697
stepin104609 <69045961+stepin104609@users.noreply.github.com>
2020-12-06 15:39:48 +0530

    Update test.c

--- Mini Project/Implementation/test/test.c
+++ Mini Project/Implementation/test/test.c
@@ -1,14 +1,38 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 #include<Header.h>
-#define PROJECT_NAME    "Playfaircipher"
+#define PROJECT_NAME    "LSB-based-Steganography"
 
-void test_check_decode_files(void);
-void test_plainedit(void);
-void test_matrixx(void);
-void test_makedistinct(void);
-void test_decrypt(void);
-void test_playfair(void);
+void check_decode_files(void);
+void read_and_validate_decode_args(void);
+void do_decoding(void);
+void open_files_for_decoding(void);
+void decode_magic_string(void);
+void get_byte_from_lsb(void);
+void decode_secret_file_extn_size(void);
+void decode_size(void);
+void decode_secret_file_extn(void);
+void decode_secret_file_size(void);
+void decode_file_size(void);
+void decode_secret_file_data(void);
+void decode_data_to_file(void);
+
+void check_operation_type(void);
+void check_encode_files(void);
+void read_and_validate_encode_args(void);
+void do_encoding(void);
+void open_files(void);
+void check_capacity(void);
+void get_file_size(void);
+void get_image_size_for_bmp(void);
+void copy_bmp_header(void);
+void encode_magic_string(void);
+void encode_data_to_image(void);
+void encode_byte_to_lsb(void);
+void encode_size(void);
+void encode_size_to_lsb(void);
+void encode_secret_file_extn(void);
+void encode_secret_file_size(void);
 
 int main() {
 
@@ -16,39 +40,69 @@ int main() {
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
   
-  CU_add_test(suite, "decplainedit", test_dcplainedit);
-  CU_add_test(suite, "plainedit", test_plainedit);
-  CU_add_test(suite, "matrixx", test_matrixx);
-  CU_add_test(suite, "makeditinct", test_mkedistinct);
-  CU_add_test(suite, "decrypt", test_decrypt);
-    CU_add_test(suite, "playfair", test_plyfair);
- 
+  CU_add_test(suite, "check_decode_files", check_decode_files);
+  CU_add_test(suite, "read_and_validate_decode_args", read_and_validate_decode_args);
+  CU_add_test(suite, "do_decoding", do_decoding);
+  CU_add_test(suite, "open_files_for_decoding", open_files_for_decoding);
+  CU_add_test(suite, "decode_magic_string", decode_magic_string);
+  CU_add_test(suite, "get_byte_from_lsb", get_byte_from_lsb);
+   CU_add_test(suite, "decode_secret_file_extn_size", decode_secret_file_extn_size);
+   CU_add_test(suite, "decode_size", decode_size);
+   CU_add_test(suite, "decode_secret_file_extn", decode_secret_file_extn);
+   CU_add_test(suite, "decode_secret_file_size", decode_secret_file_size);
+   CU_add_test(suite, "decode_file_size", decode_file_size);
+   CU_add_test(suite, "decode_secret_file_data", decode_secret_file_data);
+   CU_add_test(suite, "decode_data_to_file", decode_data_to_file);
+   CU_add_test(suite, "check_operation_type", check_operation_type);
+   CU_add_test(suite, "check_encode_files", check_encode_files);
+   CU_add_test(suite, "read_and_validate_encode_args", read_and_validate_encode_args);
+   CU_add_test(suite, "do_encoding", do_encoding);
+   CU_add_test(suite, "open_files", open_files);
+   CU_add_test(suite, "check_capacity", check_capacity);
+   CU_add_test(suite, "get_file_size",get_file_size);
+   CU_add_test(suite, "get_image_size_for_bmp", get_image_size_for_bmp);
+   CU_add_test(suite, "copy_bmp_header", copy_bmp_header);
+   CU_add_test(suite, "encode_magic_string", encode_magic_string);
+   CU_add_test(suite, "encode_data_to_image",encode_data_to_image);
+   CU_add_test(suite, "encode_byte_to_lsb",encode_byte_to_lsb);
+   CU_add_test(suite, "encode_size", encode_size);
+   CU_add_test(suite, "encode_size_to_lsb",encode_size_to_lsb);
+   CU_add_test(suite, "encode_secret_file_extn",encode_secret_file_extn);
+   CU_add_test(suite, "encode_secret_file_size",encode_secret_file_size);
+   
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
   CU_basic_run_tests();
   CU_cleanup_registry();
   return 0;
 }
-void test_decplainedit(void){
- 
- }
+void check_decode_files(void); {}
+void read_and_validate_decode_args(void); {}
+void do_decoding(void); {}
+void open_files_for_decoding(void); {}
+void decode_magic_string(void); {}
+void get_byte_from_lsb(void); {}
+void decode_secret_file_extn_size(void); {}
+void decode_size(void); {}
+void decode_secret_file_extn(void); {}
+void decode_secret_file_size(void); {}
+void decode_file_size(void); {}
+void decode_secret_file_data(void); {}
+void decode_data_to_file(void); {}
 
-void test_plainedit(void){
-  
-}
-
-void test_matrixx(void){
-  
-}
-
-void test_makedistinct(void){
-  
-}
-
-void test_decrypt(void){
-  
-}
-
-void test_playfair(void){
-  
-}
+void check_operation_type(void); {}
+void check_encode_files(void); {}
+void read_and_validate_encode_args(void); {}
+void do_encoding(void); {}
+void open_files(void); {}
+void check_capacity(void); {}
+void get_file_size(void); {}
+void get_image_size_for_bmp(void); {}
+void copy_bmp_header(void); {} 
+void encode_magic_string(void); {}
+void encode_data_to_image(void); {}
+void encode_byte_to_lsb(void); {}
+void encode_size(void); {}
+void encode_size_to_lsb(void); {}
+void encode_secret_file_extn(void); {}
+void encode_secret_file_size(void); {}
