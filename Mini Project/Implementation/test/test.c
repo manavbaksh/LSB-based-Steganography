593b4d9a9204004d45ef9f404ce001ed43fcdea1
stepin104609 <69045961+stepin104609@users.noreply.github.com>
2020-12-06 16:44:27 +0530

    Update test.c

--- Mini Project/Implementation/test/test.c
+++ Mini Project/Implementation/test/test.c
@@ -1,6 +1,9 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
-#include<Header.h>
+#include<com.h>
+#include<dec.h>
+#include<enc.h>
+#include<types.h>
 #define PROJECT_NAME    "LSB-based-Steganography"
 
 void check_decode_files(void);
@@ -76,33 +79,33 @@ int main() {
   CU_cleanup_registry();
   return 0;
 }
-void check_decode_files(void); {}
-void read_and_validate_decode_args(void); {}
-void do_decoding(void); {}
-void open_files_for_decoding(void); {}
-void decode_magic_string(void); {}
-void get_byte_from_lsb(void); {}
-void decode_secret_file_extn_size(void); {}
-void decode_size(void); {}
-void decode_secret_file_extn(void); {}
-void decode_secret_file_size(void); {}
-void decode_file_size(void); {}
-void decode_secret_file_data(void); {}
-void decode_data_to_file(void); {}
+void check_decode_files(void) {}
+void read_and_validate_decode_args(void) {}
+void do_decoding(void) {}
+void open_files_for_decoding(void) {}
+void decode_magic_string(void) {}
+void get_byte_from_lsb(void) {}
+void decode_secret_file_extn_size(void) {}
+void decode_size(void) {}
+void decode_secret_file_extn(void) {}
+void decode_secret_file_size(void) {}
+void decode_file_size(void) {}
+void decode_secret_file_data(void) {}
+void decode_data_to_file(void) {}
 
-void check_operation_type(void); {}
-void check_encode_files(void); {}
-void read_and_validate_encode_args(void); {}
-void do_encoding(void); {}
-void open_files(void); {}
-void check_capacity(void); {}
-void get_file_size(void); {}
-void get_image_size_for_bmp(void); {}
-void copy_bmp_header(void); {} 
-void encode_magic_string(void); {}
-void encode_data_to_image(void); {}
-void encode_byte_to_lsb(void); {}
-void encode_size(void); {}
-void encode_size_to_lsb(void); {}
-void encode_secret_file_extn(void); {}
-void encode_secret_file_size(void); {}
+void check_operation_type(void) {}
+void check_encode_files(void) {}
+void read_and_validate_encode_args(void) {}
+void do_encoding(void) {}
+void open_files(void) {}
+void check_capacity(void) {}
+void get_file_size(void) {}
+void get_image_size_for_bmp(void) {}
+void copy_bmp_header(void) {} 
+void encode_magic_string(void) {}
+void encode_data_to_image(void) {}
+void encode_byte_to_lsb(void) {}
+void encode_size(void) {}
+void encode_size_to_lsb(void) {}
+void encode_secret_file_extn(void) {}
+void encode_secret_file_size(void) {}
